cd81ae1dcb3b8678392fa38f10e9fdd9fa9922ac
Altenir Modesto Gomes <pandinhaAnao12@proton.me>
2024-10-22 17:38:54 -0400

    Resolvi o simple sum

---
+++ 01_basic/04_Simple_Sum.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;  
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        
+        a = input.nextInt();
+        b = input.nextInt();
+        int soma = a+b;
+        System.out.println("SOMA = "+soma);
+    }
+ 
+}
